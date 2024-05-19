d4d8a51c35aba1dd30755d64b62a4287a3a1558e
its-swats <shawnw86@gmail.com>
2024-05-19 15:07:51 -0700

    linting

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
       <div className="flex min-h-full flex-col">
         <header className="shrink-0 bg-gray-900">
           <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-            <h1 className="text-white">Doctor's Orders</h1>
+            <h1 className="text-white">Doctor&apos;s Orders</h1>
             { children }
           </div>
         </header>
